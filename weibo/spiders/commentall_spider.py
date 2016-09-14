6d31ec73cc000e1663240b92cffe32517be4791b
zz <pengzhong2010@gmail.com>
2016-09-14 15:46:36 +0800

    update

--- weibo/spiders/commentall_spider.py
+++ weibo/spiders/commentall_spider.py
@@ -18,19 +18,21 @@ from pymysql import PyMysql
 # url_fans
 # my_cookies
 
-class CommentSpider(scrapy.spiders.Spider):
-    name = "comment"
+class CommentallSpider(scrapy.spiders.Spider):
+    name = "commentall"
     allowed_domains = ['weibo.com', 'weibo.cn', 'sina.com.cn']
     # start_urls=['http://m.weibo.cn']
     surl = 'http://weibo.com/xiaopapi/profile?rightmod=1&wvr=6&mod=personnumber&is_all=1'
 
-    spider_sep_per_time = 3600
+    # spider_sep_per_time = 3600
     blog_list=''
     blog_list_len=0
     blog_list_key=0
     comment_page=1
     url_page_demo=''
     blog_id=''
+    blog_comment_page_total=0
+    blog_comment_page_current=1
 
     mysql_con = ''
     my_cookies = {}
@@ -83,6 +85,7 @@ class CommentSpider(scrapy.spiders.Spider):
         # next_url='http://weibo.com'+str(blog_dict['url'])
         self.blog_id = blog_dict['blog_id']
         self.comment_page = 1
+        self.blog_comment_page_total = 1
         next_url = self.url_page_demo + str(self.comment_page)
 
         return [
@@ -110,13 +113,26 @@ class CommentSpider(scrapy.spiders.Spider):
 
         # with open('commentpage', 'wb') as f:
         #     f.write(response.body)
+
         time_now = int(time.time())
+        time_value = time.localtime(time_now)
+        time_year_str = time.strftime('%Y', time_value)
+        time_day_str = time.strftime('%Y-%m-%d', time_value)
+        time_day_str_all = time_day_str + " 00:00:00"
+        time_s = time.mktime(time.strptime(time_day_str_all, '%Y-%m-%d %H:%M:%S'))
+        time_day = int(time_s)
 
         res = json.loads(response.body)
         aa= res.get('data')
+        page_total_dict=aa.get('page')
+        if page_total_dict:
+            page_total = page_total_dict.get('totalpage')
+            if page_total:
+                self.blog_comment_page_total = int(page_total)
         str1 = aa.get('html')
         # with open('commentpage2', 'wb') as f:
         #     f.write(str1)
+        # return
         # gbkTypeStr = bb.encode("GBK", 'ignore')
         # print gbkTypeStr
 
@@ -129,73 +145,91 @@ class CommentSpider(scrapy.spiders.Spider):
             else:
                 last_time_str = link2[0]
                 m1 = re.match(r'(.*)' + u'分钟前' + '.*', last_time_str)
+                m2 = re.match(r'.*' + u'今天' + '(.*)', last_time_str)
+                m3 = re.match(r'(.*)' + u'月' + '(.*)' + u'日' + '(.*)', last_time_str)
                 if m1:
                     last_time = m1.groups()[0]
                     last_time = last_time.strip()
                     last_time = int(last_time)
                     last_time = last_time * 60
                     data_time = time_now - last_time
+                elif m2:
+                    tmp_time = m2.groups()[0]
+                    tmp_time = tmp_time.strip()
+                    tmp_time = time_day_str + ' ' + tmp_time
+                    s = time.mktime(time.strptime(tmp_time, '%Y-%m-%d %H:%M'))
+                    data_time = int(s)
+                elif m3:
+                    if len(m3.groups()) == 3:
+                        tmp_time_month = m3.groups()[0]
+                        tmp_time_day = m3.groups()[1]
+                        tmp_time_hm = m3.groups()[2]
+                        tmp_time_hm = tmp_time_hm.strip()
+                        tmp_time_str = time_year_str + '-' + tmp_time_month + '-' + tmp_time_day + " " + tmp_time_hm
+                        s = time.mktime(time.strptime(tmp_time_str, '%Y-%m-%d %H:%M'))
+                        data_time = int(s)
+                    else:
+                        continue
+                else:
+                    continue
 
-                    comment_id = ''
-                    comment_id_list = link1.xpath('@comment_id').extract()
-                    if comment_id_list:
-                        comment_id = comment_id_list[0]
+                comment_id = ''
+                comment_id_list = link1.xpath('@comment_id').extract()
+                if comment_id_list:
+                    comment_id = comment_id_list[0]
 
-                    comment_text = ''
-                    comment_text_list = link1.xpath(
-                        'div[contains(@class, "list_con")]/div[contains(@class, "WB_text")]/text()').extract()
-                    if comment_text_list:
-                        for i in comment_text_list:
-                            i = i.strip()
-                            if i:
-                                comment_text = i
+                comment_text = ''
+                comment_text_list = link1.xpath(
+                    'div[contains(@class, "list_con")]/div[contains(@class, "WB_text")]/text()').extract()
+                if comment_text_list:
+                    for i in comment_text_list:
+                        i = i.strip()
+                        if i:
+                            comment_text = i
 
-                    comment_user_id = ''
-                    a_list = link1.xpath('div[contains(@class, "list_con")]/div[contains(@class, "WB_text")]/a')
-                    if a_list:
-                        comment_user_id_list = a_list[0].xpath('@usercard').extract()
-                        if comment_user_id_list:
-                            comment_user_id_str = comment_user_id_list[0]
-                            comment_user_id = comment_user_id_str[3:]
-                    # print self.blog_id
-                    # print data_time
-                    # print comment_id
-                    # print comment_text
-                    # print comment_user_id
-                    self.output_comment(self.blog_id, comment_id, comment_user_id, comment_text, data_time)
+                comment_user_id = ''
+                a_list = link1.xpath('div[contains(@class, "list_con")]/div[contains(@class, "WB_text")]/a')
+                if a_list:
+                    comment_user_id_list = a_list[0].xpath('@usercard').extract()
+                    if comment_user_id_list:
+                        comment_user_id_str = comment_user_id_list[0]
+                        comment_user_id = comment_user_id_str[3:]
+                # print self.blog_id
+                # print data_time
+                # print comment_id
+                # print comment_text
+                # print comment_user_id
+                self.output_comment(self.blog_id, comment_id, comment_user_id, comment_text, data_time)
 
-        link3 = list1[-1].xpath(
-            'div[contains(@class, "list_con")]/div[contains(@class, "WB_func clearfix")]/div[contains(@class, "WB_from S_txt2")]/text()').extract()
-        if link3:
-            data_time_str = link3[0]
-            m1 = re.match(r'(.*' + u'分钟前' + '.*)', data_time_str)
-            if m1:
-                # next page
-                # pass
-                self.comment_page = self.comment_page + 1
-                next_url = self.url_page_demo + str(self.comment_page)
-                time.sleep(1)
-                return [
-                    scrapy.Request(url=next_url, meta={'cookiejar': 0}, cookies=self.my_cookies, dont_filter=True,
-                                   callback=self.page_parse
-                                   )]
 
-        #next blog
-        # pass
-        blog_dict = self.get_blog_one()
-        if not blog_dict:
-            return
-        # catch
-        self.url_page_demo = 'http://weibo.com/aj/v6/comment/big?ajwvr=6&id=' + str(blog_dict['blog_id']) + '&page='
-        # next_url='http://weibo.com'+str(blog_dict['url'])
-        self.blog_id = blog_dict['blog_id']
-        self.comment_page = 1
-        next_url = self.url_page_demo + str(self.comment_page)
-        time.sleep(1)
-        return [
-            scrapy.Request(url=next_url, meta={'cookiejar': 0}, cookies=self.my_cookies, dont_filter=True,
-                           callback=self.page_parse
-                           )]
+        self.comment_page = self.comment_page + 1
+        if self.comment_page <= self.blog_comment_page_total:
+            next_url = self.url_page_demo + str(self.comment_page)
+            time.sleep(1)
+            return [
+                scrapy.Request(url=next_url, meta={'cookiejar': 0}, cookies=self.my_cookies, dont_filter=True,
+                               callback=self.page_parse
+                               )]
+
+        else:
+
+            #next blog
+            # pass
+            blog_dict = self.get_blog_one()
+            if not blog_dict:
+                return
+            # catch
+            self.url_page_demo = 'http://weibo.com/aj/v6/comment/big?ajwvr=6&id=' + str(blog_dict['blog_id']) + '&page='
+            # next_url='http://weibo.com'+str(blog_dict['url'])
+            self.blog_id = blog_dict['blog_id']
+            self.comment_page = 1
+            self.blog_comment_page_total = 1
+            next_url = self.url_page_demo + str(self.comment_page)
+            time.sleep(1)
+            return [
+                scrapy.Request(url=next_url, meta={'cookiejar': 0}, cookies=self.my_cookies, dont_filter=True,
+                               callback=self.page_parse
+                               )]
 
 
 
@@ -251,20 +285,20 @@ class CommentSpider(scrapy.spiders.Spider):
                 self.blog_list_len = 0
                 self.blog_list_key = 0
                 #sleep
-                self.rest()
-                return self.get_blog_one()
-                pass
+                # self.rest()
+                # return self.get_blog_one()
+                return
         return dict_tmp
     def get_blog_list(self):
-        sql = " select * from weibo_blog where appid = "+str(self.appid)+" order by create_time desc limit 20"
+        sql = " select * from weibo_blog where appid = "+str(self.appid)+" order by create_time desc limit 1"
         ret = self.mysql_con.select(sql)
         if ret:
             self.blog_list=ret
             self.blog_list_len=len(ret)
 
 
-    def rest(self):
-        time.sleep(self.spider_sep_per_time)
+    # def rest(self):
+    #     time.sleep(self.spider_sep_per_time)
 
     def output_comment(self, blog_id, comment_id, comment_user_id, comment_text, datatime):
         date_now = time.strftime('%Y-%m-%d', time.gmtime(time.time()))
