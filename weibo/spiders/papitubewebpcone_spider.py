a6ed28b3846faeee93753abad4a3854c75754bc6
pz <pengzhong2010@gmail.com>
2016-11-25 13:34:08 +0800

    update

--- weibo/spiders/papitubewebpcone_spider.py
+++ weibo/spiders/papitubewebpcone_spider.py
@@ -7,11 +7,16 @@ class PapitubewebpconeSpider(webpcone_spider.WebpconeSpider):
     name = "papitubewebpcone"
     appid = 2806796
     spider_sep_per_time = 300
-    surl = 'http://weibo.com/p/1005055932557435/myfollow?relate=fans#place'
-    first_url_prefix = 'http://weibo.com/p/1005055932557435/myfollow?cfs=600&relate=fans&t=1&f=1&type=&Pl_Official_RelationFans__91_page='
-    first_url_suffix = '#Pl_Official_RelationFans__91'
-    second_url_prefix = 'http://weibo.com/p/1005055932557435/myfollow?cfs=600&relate=fans&t=1&f=1&type=&Pl_Official_RelationFans__91_page='
-    second_url_suffix = '#Pl_Official_RelationFans__91'
+    # surl = 'http://weibo.com/p/1005055932557435/myfollow?relate=fans#place'
+    surl = 'http://weibo.com/p/1005055932557435/follow?relate=fans&page=1#Pl_Official_HisRelation__61'
+    # first_url_prefix = 'http://weibo.com/p/1005055932557435/myfollow?cfs=600&relate=fans&t=1&f=1&type=&Pl_Official_RelationFans__91_page='
+    # first_url_suffix = '#Pl_Official_RelationFans__91'
+    # second_url_prefix = 'http://weibo.com/p/1005055932557435/myfollow?cfs=600&relate=fans&t=1&f=1&type=&Pl_Official_RelationFans__91_page='
+    # second_url_suffix = '#Pl_Official_RelationFans__91'
+    first_url_prefix = 'http://weibo.com/p/1005055932557435/follow?relate=fans&page='
+    first_url_suffix = '#Pl_Official_HisRelation__61'
+    second_url_prefix = 'http://weibo.com/p/1005055932557435/follow?relate=fans&page='
+    second_url_suffix = '#Pl_Official_HisRelation__61'
     login_uid = 0
     uid_tmp_list = []
 
